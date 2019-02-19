00967efa4c9f173b9a48f9e2af1d1da37db62cc9
Rafael Angelo Pucut <38448911+Rancelot@users.noreply.github.com>
2019-02-19 12:45:28 -0700

    React Lab startup template
    css and js files copied

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HeaderApp from './components/HeaderApp.js';
+import PhotoBrowser from './components/PhotoBrowser.js';
+import * as cloneDeep from 'lodash/cloneDeep';
+import { Route } from 'react-router-dom';
+import Home from './components/Home.js';
+import About from './components/About.js';
+import Favorites from './components/Favorites.js';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props); 
+    this.state = { photos: [], favorites: [] };
+  }
+  async componentDidMount() {
+      try {
+          const url = "http://randyconnolly.com/funwebdev/services/travel/images.php";
+          const response = await fetch(url);
+          const jsonData = await response.json();
+          this.setState( {photos: jsonData} );
+      } catch (error) {
+          console.error(error);
+      }
+  }
+    
+  updatePhoto = (id, photo) => {
+      //create deep clone of photo array from state
+      //we will use a lodash function for that task
+      const copyPhotos = cloneDeep(this.state.photos);
+      //find a photo to update in cloned array
+      const photoToReplace = copyPhotos.find( p => p.id === id);
+      //replace photo fields with edited values
+      photoToReplace.title = photo.title;
+      photoToReplace.city = photo.city;
+      photoToReplace.country = photo.country;
+      //update state
+      this.setState( {photos: copyPhotos } );
+  }
+  
+  addPhotoToFavorites = (id) => {
+      let photoArray = this.state.photos;
+      let findPhotoData = photoArray.find( p => p.id === id);
+      let tempFav = this.state.favorites;
+      tempFav.push(findPhotoData);
+      this.setState({favorites: tempFav});
+  }
+  
   render() {
     return (
-        <div className="App">
-        <header className="App-header">
-          <h1>Testing Github</h1>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+            <HeaderApp />
+            <Favorites photos={this.state.photos} favorites={this.state.favorites} />
+            <Route path='/' exact component={Home} />
+            <Route path='/home' exact component={Home} />
+            <Route path='/browse' exact render={ (props) => 
+                <PhotoBrowser photos={this.state.photos} updatePhoto={this.updatePhoto} addPhotoToFavorites={this.addPhotoToFavorites} /> 
+            }
+            />
+            <Route path='/about' exact component={About} />
+        </div>
     );
   }
 }

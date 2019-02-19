97b5d655f1703bf42365d5805e04e83c7a2ac52e
Rafael Angelo Pucut <38448911+Rancelot@users.noreply.github.com>
2019-02-19 13:15:52 -0700

    About Page Edited
    More detail and HeaderApp component Imported

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -1,12 +1,23 @@
 import React from "react";
 import './Home.css';
+import HeaderApp from './HeaderApp.js';
 class About extends React.Component {
     render() {
         return (
             <div className="banner">
+                <HeaderApp />
                 <div>
                     <h1>About</h1>
-                    <h3>Contains About Component</h3>
+                    <h3>Course: COMP 4513 - WEB 3</h3>
+                    <h3>Members: John Kevin Ruiz & Rafael Angelo Pucut</h3>
+                    <h3>Technologies Used</h3>
+                    <h4>
+                        <ul>
+                            <li>React</li>
+                            <li>JavaScript</li>
+                            <li>Node.js</li>
+                        </ul>
+                    </h4>
                 </div>
             </div>
         );

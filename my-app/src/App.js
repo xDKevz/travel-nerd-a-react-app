df436570d8affaf814733bc56063068f601a322a
Rafael Angelo Pucut <38448911+Rancelot@users.noreply.github.com>
2019-02-19 12:24:32 -0700

    Summary Test
    added h1 tag

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,8 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+        <div className="App">
         <header className="App-header">
+          <h1>Testing Github</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.

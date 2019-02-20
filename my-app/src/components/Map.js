e25fe492ab4670cf86e7686c3b1ab0142791eb70
Kevin Ruiz <26445418+xDKevz@users.noreply.github.com>
2019-02-20 15:51:25 -0700

    Map not done

--- my-app/src/components/Map.js
+++ my-app/src/components/Map.js
@@ -6,26 +6,26 @@ export class MapContainer extends React.Component {
 
     
     render() {
-        const style = { width: '900px', height: '900px' };
+        const style = { width: '500px', height: '800px' };
         let photo = this.props.photos.find((obj) => obj.id === this.props.currentPhoto);
         console.log(photo);
 
         if (photo != null) {
             return (
-                <Map key={photo.id} google={this.props.google} style={style}
-                    initialCenter={{ lat: Number(photo.latitude), lng: Number(photo.longitude) }}
-                    zoom={15} onClick={this.onMapClicked}
-                >
+                <div className="map" >
+                    <div>
+                        <Map key={photo.id} google={this.props.google} style={style}
+                            initialCenter={{ lat: Number(photo.latitude), lng: Number(photo.longitude) }}
+                            zoom={15} onClick={this.onMapClicked}
+                        >
 
-                    <Marker onClick={this.onMarkerClick}
-                        name={'Current location'} />
-
-                    <InfoWindow onClose={this.onInfoWindowClose}>
-                        <div>
-
-                        </div>
-                    </InfoWindow>
-                </Map>
+                        <Marker title={photo.title} onClick={this.onMarkerClick} name={'Current location'} />
+                        </Map>
+                    </div>
+                    <div>
+                        <h1>This is a random text</h1>
+                    </div>
+                </div>
             );
         } else {
             return (<Map google={this.props.google} style={style}

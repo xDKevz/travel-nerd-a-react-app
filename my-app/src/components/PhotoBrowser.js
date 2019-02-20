de903dbbbac7f7596b1bca06ff653c0cff7e7bfb
Kevin Ruiz <26445418+xDKevz@users.noreply.github.com>
2019-02-20 11:57:58 -0700

    PhotoBrowser.js update
    change render code to be more concise and remove duplicate code

--- my-app/src/components/PhotoBrowser.js
+++ my-app/src/components/PhotoBrowser.js
@@ -18,32 +18,25 @@ class PhotoBrowser extends React.Component {
     changeRender = (value) =>    {
         this.setState({renderValue: value});
     }
+
+    viewOrEdit = () => {
+        if (this.state.renderValue === "edit")
+            return (<EditPhotoDetails changeRender={this.changeRender} photos={this.props.photos} currentPhoto={this.state.currentPhoto} updatePhoto={this.props.updatePhoto} />);
+        else 
+            return (<ViewSinglePhoto changeRender={this.changeRender} photos={this.props.photos} currentPhoto={this.state.currentPhoto} />);
+    }
     
     render() {
-        if (this.state.renderValue === "view") {
-            return (
-                <div>
-                    <HeaderApp />
-                    <Favorites favorites={this.props.favorites} />
-                    <section className="container">
+        return (
+            <div>
+                <HeaderApp />
+                <Favorites favorites={this.props.favorites} />
+                <section className="container">
                         <PhotoList removePhotoFromFav={this.props.removePhotoFromFav} removePhotoFromList={this.props.removePhotoFromList} sortByValue={this.props.sortByValue} photos={this.props.photos} showImageDetails={this.showImageDetails} addToFavorites={this.props.addPhotoToFavorites} />
-                        <ViewSinglePhoto changeRender={this.changeRender} photos={this.props.photos} currentPhoto={this.state.currentPhoto} />
-                    </section>
-                </div>
-            );
-        } else {
-            return (
-                <div>
-                    <HeaderApp />
-                    <Favorites favorites={this.props.favorites} />
-                    <section className="container">
-                        <PhotoList removePhotoFromFav={this.props.removePhotoFromFav} removePhotoFromList={this.props.removePhotoFromList} sortByValue={this.props.sortByValue} photos={this.props.photos} showImageDetails={this.showImageDetails} addToFavorites={this.props.addPhotoToFavorites} />
-                        <EditPhotoDetails changeRender={this.changeRender} photos={this.props.photos} currentPhoto={this.state.currentPhoto} updatePhoto={this.props.updatePhoto} />
-                    </section>
-                </div>
-            );
-        }
-        
+                        {this.viewOrEdit()}
+                </section>
+            </div>
+        );
     }
 }
 
--- my-app/src/components/PhotoThumb.js
+++ my-app/src/components/PhotoThumb.js
@@ -20,20 +20,22 @@ class PhotoThumb extends React.Component {
     render() {
         const imgURL = `https://storage.googleapis.com/funwebdev-3rd-travel/square-medium/${this.props.photo.path}`;
         return (
-            
-                <div className="photoBox" onClick={ this.handleViewClick } >
-                    
-                    <figure>
-                        <img src={imgURL} className="photoThumb" title={this.props.photo.title} alt={this.props.photo.title} />
-                    </figure>
-                    <div>
+                
+                <div>
                     <button onClick={this.handleRemoveClick}>X</button>
-                        <h3>{this.props.photo.title}</h3>
-                        <p>{this.props.photo.city}, {this.props.photo.country}</p>
-                        <button onClick={this.handleViewClick} >View</button>
-                        <button onClick={this.handleFavClick}>❤</button>
-                        <button>Map</button>
-                        <button>Edit</button>
+                    <div  className="photoBox" onClick={ this.handleViewClick } >
+                        <figure>
+                            <img src={imgURL} className="photoThumb" title={this.props.photo.title} alt={this.props.photo.title} />
+                        </figure>
+                        <div>
+                            
+                            <h3>{this.props.photo.title}</h3>
+                            <p>{this.props.photo.city}, {this.props.photo.country}</p>
+                            <button onClick={this.handleViewClick} >View</button>
+                            <button onClick={this.handleFavClick}>❤</button>
+                            <button>Map</button>
+                            <button>Edit</button>
+                        </div>
                     </div>
                 </div>
         );

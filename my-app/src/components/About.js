8692163279ffe5e236e78bde6d670c14456a83fc
Kevin Ruiz <26445418+xDKevz@users.noreply.github.com>
2019-02-26 21:33:07 -0700

    About.js

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -18,33 +18,40 @@ class About extends React.Component {
                         <p className="para1">
                             This is a website that demonstrates our understanding of how to generate a single-page application using React.
                         </p>
-                        <h3>Team Members (Github):</h3>
+                        <h3>Team Members:</h3>
                         <div className="memojis">  
                             <a href="https://github.com/xDKevz" target="_blank">
-                                <img src={memojiK} width="200" height="200" alt="Kevin" />
-                                <h5>John Kevin Ruiz</h5>
+                                <figure>
+                                    <img src={memojiK} width="200" height="200" alt="Kevin" />
+                                    <figcaption>John Kevin Ruiz</figcaption>
+                                </figure>
                             </a>
                             
                             <a href="https://github.com/Rancelot" target="_blank">
-                                <img src={memojiR} width="200" height="200" alt="Rafael" />
-                                <h5>Rafael Angelo Pucut</h5>
+                                <figure>
+                                    <img src={memojiR} width="200" height="200" alt="Rafael" />
+                                    <figcaption>Rafael Angelo Pucut</figcaption>
+                                </figure>
                             </a>
+                            <br></br>
                         </div>
-                        <h4>Helpful Resources:</h4>
+                        <h3>CREDITS AND OTHER RESOURCES</h3>
                         <div>
-                            <li><a href="https://codepen.io/peternguyen/pen/hICga/">Favorites Toggle</a></li>
-                            <li><a href="https://www.npmjs.com/package/google-maps-react">Google Maps React</a></li>
-                            <li><a href="https://www.movable-type.co.uk/scripts/latlong.html">Haversine Formula JavaScript</a></li>
-                            <li><a href="https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=en">CORS Extension</a></li>
-                            <li><a href="https://stuk.github.io/jszip/documentation/examples/downloader.html">JSZip Helpful Code</a></li>
+                            <ul className="resources">
+                                <li><a href="https://codepen.io/peternguyen/pen/hICga/">Favorites Toggle</a></li>
+                                <li><a href="https://www.npmjs.com/package/google-maps-react">Google Maps React</a></li>
+                                <li><a href="https://www.movable-type.co.uk/scripts/latlong.html">Haversine Formula JavaScript</a></li>
+                                <li><a href="https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=en">CORS Extension</a></li>
+                                <li><a href="https://stuk.github.io/jszip/documentation/examples/downloader.html">JSZip Helpful Code</a></li>
+                            </ul>
                         </div>
-                        <h3>Powered BY:</h3>
+                        <h3>Technology use:</h3>
                         <div className="powered">
-                            <p><img src="https://nodejs.org/static/images/logo.svg" width="200" height="200" alt="NodeJsLogo" /></p>
-                            <p><img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" width="200" height="200" alt="ReactLogo" /></p>
-                            <p><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" width="200" height="200" alt="JSLogo" /></p>
-                            <p><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" width="200" height="200" alt="GithubLogo" /></p>
-                            <p><img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" width="200" height="200" alt="GoogleLogo" /></p>
+                            <p><a href="https://nodejs.org/en/"><img src="https://nodejs.org/static/images/logo.svg" width="200" height="200" alt="NodeJsLogo" /></a></p>
+                            <p><a href="https://reactjs.org/"><img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" width="200" height="200" alt="ReactLogo" /></a></p>
+                            <p><a href="https://www.javascript.com/"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" width="200" height="200" alt="JSLogo" /></a></p>
+                            <p><a href="https://www.github.com/"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" width="200" height="200" alt="GithubLogo" /></a></p>
+                            <p><a href="https://cloud.google.com/maps-platform/"><img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" width="200" height="200" alt="GoogleLogo" /></a></p>
                         </div>
                     </div>
                 </div>
